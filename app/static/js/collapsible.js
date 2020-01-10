b25b9d677e7d24e9edb3eb489ecbc49c689cd251
Jessica Nash <janash@vt.edu>
2020-01-10 12:28:07 -0500

    flowcharts can now be viewed using ajaxload

--- app/static/js/collapsible.js
+++ app/static/js/collapsible.js
@@ -1,10 +1,10 @@
 
-var coll = document.getElementsByClassName("collapsible");
+var coll = document.getElementsByClassName("collapsible_description");
 var i;
 
 for (i = 0; i < coll.length; i++) {
 coll[i].addEventListener("click", function() {
-    this.classList.toggle("active");
+    this.classList.toggle("expand");
     var content = this.nextElementSibling;
     if (content.style.display === "block") {
     content.style.display = "none";
--- app/templates/js/flowchart.js
+++ app/templates/js/flowchart.js
@@ -1,84 +1,80 @@
 
-  window.addEventListener('DOMContentLoaded', function(){
 
-      var cy = window.cy = cytoscape({
-        container: document.getElementById('cy'),
+var cy = window.cy = cytoscape({
+  container: document.getElementById('cy'),
 
-        boxSelectionEnabled: false,
-        autounselectify: true,
+  boxSelectionEnabled: false,
+  autounselectify: true,
 
-        layout: {
-          name: 'preset'
-        },
+  layout: {
+    name: 'preset'
+  },
 
-        style: [
-          {
-            selector: 'node',
-            style: {
-              'shape': 'rectangle',
-              'background-color': '#DCDCDC',
-              'label': 'data(name)',
-              'text-halign': 'center',
-              'text-valign': 'center',
-              'width': 200,
-            }
-          },
+  style: [
+    {
+      selector: 'node',
+      style: {
+        'shape': 'rectangle',
+        'background-color': '#DCDCDC',
+        'label': 'data(name)',
+        'text-halign': 'center',
+        'text-valign': 'center',
+        'width': 200,
+      }
+    },
 
-          {
-            selector: 'edge',
-            style: {
-              'width': 4,
-              'target-arrow-shape': 'triangle',
-              'line-color': '#696969',
-              'target-arrow-color': '#696969',
-              'curve-style': 'bezier',
-            }
-          }
-        ],
+    {
+      selector: 'edge',
+      style: {
+        'width': 4,
+        'target-arrow-shape': 'triangle',
+        'line-color': '#696969',
+        'target-arrow-color': '#696969',
+        'curve-style': 'bezier',
+      }
+    }
+  ],
 
-        elements:
-        [
-          {% for element in data %}
+  elements:
+  [
+    {% for element in data %}
 
-            {
-            {% for key, value in element.items() %}
-                {%- if value is mapping -%}
-                  '{{ key|safe }}' : {{ value|safe }},
-                {%- else -%}
-                  '{{ key|safe }}' : '{{ value|safe }}',
-                {% endif %}
-            {% endfor %}
-            },
-          {%- endfor -%}
-        ]
+      {
+      {% for key, value in element.items() %}
+          {%- if value is mapping -%}
+            '{{ key|safe }}' : {{ value|safe }},
+          {%- else -%}
+            '{{ key|safe }}' : '{{ value|safe }}',
+          {% endif %}
+      {% endfor %}
+      },
+    {%- endfor -%}
+  ]
+});
+
+cy.nodes('[name = "Join"]').style( {
+        'shape': 'ellipse',
+        'background-color': '#000000',
+        'text-halign': 'right',
+        'text-valign': 'center',
+        'width': 30,
       });
 
-      cy.nodes('[name = "Join"]').style( {
-              'shape': 'ellipse',
-              'background-color': '#000000',
-              'text-halign': 'right',
-              'text-valign': 'center',
-              'width': 30,
-            });
-
-      cy.nodes('[name = "Start"]').style( {
-              'shape': 'ellipse',
-            });
-
-      
-      cy.on('tap', 'node', function(){
-
-        if (this.data('url') != '#') {
-  
-        try { // your browser may block popups
-          window.open(this.data('url'))
-        } catch(e){ // fall back on url change
-          window.location.href = this.data('url');
-        }
-
-        }
-
-      });     
-
-
+cy.nodes('[name = "Start"]').style( {
+        'shape': 'ellipse',
       });
+
+
+cy.on('tap', 'node', function(){
+
+  if (this.data('url') != '#') {
+
+  try { // your browser may block popups
+    window.open(this.data('url'))
+  } catch(e){ // fall back on url change
+    window.location.href = this.data('url');
+  }
+
+  }
+
+});     
