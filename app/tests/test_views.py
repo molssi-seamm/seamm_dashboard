10d9a59755582b025a66bf5b51e9e45925a74c4a
Paul Saxe <paul@thesaxes.net>
2020-04-28 16:54:25 -0400

    Added code to handle incompatible change Python 3.8 on Mac
    Also cleaned up the formatting to meet PEP-8 suggestions.

--- app/tests/test_views.py
+++ app/tests/test_views.py
@@ -7,6 +7,8 @@ from flask import url_for
 import pytest
 import requests
 import os
+import platform
+import sys
 
 import urllib.parse
 
@@ -14,11 +16,18 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
+# Needed for Python 3.8 since default for OS/X changed to spawn rather than
+# fork.
+if sys.version_info >= (3, 8) and platform.system() == 'Darwin':
+    import multiprocessing
+    multiprocessing.set_start_method("fork")
+
 
 @pytest.mark.usefixtures('live_server')
 class TestLiveServer:
     """
-    This uses selenium and the 'live_server' fixture which comes with pytest-flask.
+    This uses selenium and the 'live_server' fixture which comes with
+    pytest-flask.
     """
 
     @property
@@ -32,15 +41,18 @@ class TestLiveServer:
         expected_values = "0 2 0 1 0".split()
         displayed_values = [x for x in displayed_values if x != '']
 
-        for i,value in enumerate(displayed_values):
+        for i, value in enumerate(displayed_values):
             assert expected_values[i] == value.get_attribute('innerHTML')
-            
+
     def test_jobs_list(self, app, chrome_driver):
         chrome_driver.get(f"{self.base_url}#jobs")
-        
-        # Get the jobs table. Will want to wait for this to be loaded, of course.
-        jobs_table = WebDriverWait(chrome_driver, 20).until(EC.presence_of_element_located((By.ID, "jobs")))
-        
+
+        # Get the jobs table. Will want to wait for this to be loaded,
+        # of course.
+        jobs_table = WebDriverWait(chrome_driver, 20).until(
+            EC.presence_of_element_located((By.ID, "jobs"))
+        )
+
         # Check table dimensions.
         table_headings = jobs_table.find_elements_by_tag_name("th")
         table_rows = jobs_table.find_elements_by_tag_name("tr")
@@ -50,12 +62,13 @@ class TestLiveServer:
         # Check the response code of the links in the table.
         table_links = jobs_table.find_elements_by_class_name('nav-link')
         for link in table_links:
-            # This is just a thing we have to do because of the way 'nav-links' are handled in the app.
+            # This is just a thing we have to do because of the way 'nav-links'
+            # are handled in the app.
             important_url = link.get_attribute('href')[len(self.base_url):]
             actual_url = f'{self.base_url}/#{important_url}'
             response = requests.get(actual_url)
             assert response.status_code == 200
-            
+
         #chrome_driver.get_screenshot_as_file('test_screenshot.png')
 
     def test_job_report_file_tree(self, app, chrome_driver):
@@ -64,50 +77,71 @@ class TestLiveServer:
         """
         # Get page with chromedriver.
         chrome_driver.get(f"{self.base_url}#jobs/1")
-        
+
         # Set up samples for comparison.
         dir_path = os.path.dirname(os.path.realpath(__file__))
-        test_dir = os.path.realpath(os.path.join(dir_path, "..", "..", "data", "projects", "MyProject", "Job_000001"))
-        
+        test_dir = os.path.realpath(
+            os.path.join(
+                dir_path, "..", "..", "data", "projects", "MyProject",
+                "Job_000001"
+            )
+        )
+
         num_files = len(os.listdir(test_dir))
-        
-        # Get the file tree. Wait for a specific element to load so we know the tree is loaded.
+
+        # Get the file tree. Wait for a specific element to load so we know the
+        # tree is loaded.
         file_tree = chrome_driver.find_element_by_id('js-tree')
-        
+
         dir_path = os.path.dirname(os.path.realpath(__file__))
-        test_file = os.path.realpath(os.path.join(dir_path, "..", "..", "data", "projects", "MyProject", "Job_000001", "job.out"))
-        test_file_id = urllib.parse.quote(test_file, safe='')+'_anchor'
-        WebDriverWait(chrome_driver, 20).until(EC.presence_of_element_located((By.ID, test_file_id)))
+        test_file = os.path.realpath(
+            os.path.join(
+                dir_path, "..", "..", "data", "projects", "MyProject",
+                "Job_000001", "job.out"
+            )
+        )
+        test_file_id = urllib.parse.quote(test_file, safe='') + '_anchor'
+        WebDriverWait(chrome_driver, 20).until(
+            EC.presence_of_element_located((By.ID, test_file_id))
+        )
 
         # Now get components.
         js_tree_contents = file_tree.find_elements_by_tag_name('li')
-    
+
         num_files_in_tree = len(js_tree_contents)
 
-        assert num_files_in_tree == num_files+1
-
+        assert num_files_in_tree == num_files + 1
 
     def test_job_report_file_content(self, app, chrome_driver):
         """
         Test to click file and make sure it is loaded into div.
         """
-        
+
         # Set up sample file for comparison.
         dir_path = os.path.dirname(os.path.realpath(__file__))
-        test_file = os.path.realpath(os.path.join(dir_path, "..", "..", "data", "projects", "MyProject", "Job_000001", "job.out"))
+        test_file = os.path.realpath(
+            os.path.join(
+                dir_path, "..", "..", "data", "projects", "MyProject",
+                "Job_000001", "job.out"
+            )
+        )
         with open(test_file) as f:
             file_contents = f.read()
             file_contents_split = file_contents.split()
-        
-        test_file_id = urllib.parse.quote(test_file, safe='')+'_anchor'
-        
+
+        test_file_id = urllib.parse.quote(test_file, safe='') + '_anchor'
+
         chrome_driver.get(f"{self.base_url}#jobs/1")
 
         # Initially, there should be nothing in the text box.
-        initial_displayed_text = chrome_driver.find_element_by_id('file-content').text
-        
+        initial_displayed_text = chrome_driver.find_element_by_id(
+            'file-content'
+        ).text
+
         # Get a link for a file and click on it.
-        job_link = WebDriverWait(chrome_driver, 20).until(EC.presence_of_element_located((By.ID, test_file_id)))
+        job_link = WebDriverWait(chrome_driver, 20).until(
+            EC.presence_of_element_located((By.ID, test_file_id))
+        )
         job_link.click()
 
         # When clicked, file text should be displayed in the div.
@@ -122,28 +156,40 @@ class TestLiveServer:
 
     def test_job_report_file_content_resize(self, app, chrome_driver):
         """
-        Test to make sure file content element resizes when next element is clicked.
+        Test to make sure file content element resizes when next element is
+        clicked.
         """
 
         dir_path = os.path.dirname(os.path.realpath(__file__))
-        first_file = os.path.realpath(os.path.join(dir_path, "..", "..", "data", "projects", "MyProject", "Job_000001", "job.out"))
-        second_file = os.path.realpath(os.path.join(dir_path, "..", "..", "data", "projects", "MyProject", "Job_000001", "flowchart.flow"))
-        
-        first_file_id = urllib.parse.quote(first_file, safe='')+'_anchor'
-        second_file_id = urllib.parse.quote(second_file, safe='')+'_anchor'
+        first_file = os.path.realpath(
+            os.path.join(
+                dir_path, "..", "..", "data", "projects", "MyProject",
+                "Job_000001", "job.out"
+            )
+        )
+        second_file = os.path.realpath(
+            os.path.join(
+                dir_path, "..", "..", "data", "projects", "MyProject",
+                "Job_000001", "flowchart.flow"
+            )
+        )
+
+        first_file_id = urllib.parse.quote(first_file, safe='') + '_anchor'
+        second_file_id = urllib.parse.quote(second_file, safe='') + '_anchor'
 
         chrome_driver.get(f"{self.base_url}#jobs/1")
-        
+
         # Get a link for a file and click on it.
-        job_link = WebDriverWait(chrome_driver, 20).until(EC.presence_of_element_located((By.ID, first_file_id)))
+        job_link = WebDriverWait(chrome_driver, 20).until(
+            EC.presence_of_element_located((By.ID, first_file_id))
+        )
         job_link.click()
 
-        flowchart_link = WebDriverWait(chrome_driver, 20).until(EC.presence_of_element_located((By.ID, second_file_id)))
+        flowchart_link = WebDriverWait(chrome_driver, 20).until(
+            EC.presence_of_element_located((By.ID, second_file_id))
+        )
         flowchart_link.click()
-        
+
         # File content div should be 0 if another file type is selected.
-        assert chrome_driver.find_element_by_id('file-content').size['height'] == 0
-
-
-        
-        
+        assert chrome_driver.find_element_by_id('file-content'
+                                               ).size['height'] == 0
