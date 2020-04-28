c34dc89f16445570e3b5dc0ec8ea3d9676314649
Paul Saxe <paul@thesaxes.net>
2020-04-28 16:55:24 -0400

    Made a run() method to try to support console scripts.

--- results_dashboard.py
+++ results_dashboard.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python
 from app import create_app
 
-
-if __name__ == "__main__":
+def run():
     app = create_app()
     app.run(debug=True, use_reloader=False)
+
+if __name__ == "__main__":
+    run()
