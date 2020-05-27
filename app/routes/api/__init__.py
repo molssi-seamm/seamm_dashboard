fd2b15f5e33f3cfe38aec999842f6cf4d033c2b0
Paul Saxe <paul@thesaxes.net>
2020-05-27 14:25:18 -0400

    Added status api and prevented PEP-8 warnings.

--- app/routes/api/__init__.py
+++ app/routes/api/__init__.py
@@ -1,3 +1,4 @@
-from .jobs import *
-from .flowcharts import *
-from .projects import *
+from .jobs import *  # noqa: F401, F403
+from .flowcharts import *  # noqa: F401, F403
+from .projects import *  # noqa: F401, F403
+from .status import *  # noqa: F401, F403
