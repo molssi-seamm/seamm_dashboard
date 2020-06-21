7e88cc88f42af64468e87439426924ad63d9eefc
Jessica Nash <janash@vt.edu>
2020-06-21 11:40:59 -0400

    change to reload automatically when change to code is made

--- results_dashboard.py
+++ results_dashboard.py
@@ -4,7 +4,7 @@ from app import create_app
 
 def run():
     app = create_app()
-    app.run(debug=True, use_reloader=False)
+    app.run(debug=True, use_reloader=True)
 
 
 if __name__ == "__main__":
