73326459425458d5d6720fc7ec94eb1aea78fcdb
Jessica Nash <janash@vt.edu>
2020-09-03 12:37:27 -0400

    add api endpoint for retreiving jobs associated with a single project

--- app/routes/api/projects.py
+++ app/routes/api/projects.py
@@ -2,10 +2,10 @@
 API calls for projects
 """
 
-from app.models import Project, ProjectSchema
+from app.models import Project, ProjectSchema, Job, JobSchema
 from flask import Response
 
-__all__ = ['get_projects', 'get_project', 'list_projects']
+__all__ = ['get_projects', 'get_project', 'list_projects', 'get_project_jobs']
 
 
 def get_projects(description=None, limit=None):
@@ -41,6 +41,29 @@ def get_project(id):
 
     project_schema = ProjectSchema(many=False)
     return project_schema.dump(project), 200
+
+def get_project_jobs(id):
+    """
+    Function for api endpoint api/projects/{id}/jobs. Get jobs associated with a project.
+
+
+    Parameters
+    ----------
+    id : the ID of the project to return
+    """
+
+    project = Project.query.get(id)
+
+    if project is None:
+        return Response(status=404)
+    
+    jobs = []
+    for job in project.jobs:
+        jobs.append(Job.query.get(job.id))
+    
+    jobs_schema = JobSchema(many=True)
+
+    return jobs_schema.dump(jobs), 200
 
 
 def list_projects():
