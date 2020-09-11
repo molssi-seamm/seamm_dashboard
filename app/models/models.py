e0fd1fcebe5f3269ee366856dcdb4351d2e853a8
Jessica Nash <janash@vt.edu>
2020-09-11 11:31:11 -0400

    start models for flask-authorize

--- app/models/models.py
+++ app/models/models.py
@@ -17,13 +17,21 @@ from app import db, login_manager
 #
 #############################
 
+# Authentication Mapping Tables
+
 user_group = db.Table(
     'user_group',
     db.Column('user', db.Integer, db.ForeignKey('user.id'), primary_key=True),
-    db.Column(
-        'group', db.Integer, db.ForeignKey('group.id'), primary_key=True
-    )
+    db.Column('group', db.Integer, db.ForeignKey('group.id'), primary_key=True)
 )
+
+user_role = db.Table(
+    'user_role',
+    db.Column('user', db.Integer, db.ForeignKey('user.id'), primary_key=True),
+    db.Column('role', db.Integer, db.ForeignKey('role.id'), primary_key=True)
+)
+
+
 
 flowchart_project = db.Table(
     'flowchart_project',
@@ -68,6 +76,9 @@ class User(db.Model, UserMixin):
     added = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
     status = db.Column(db.String, default='active')
 
+    roles = db.relationship(
+        'Role', secondary=user_role, back_populates='users'
+    )
     groups = db.relationship(
         'Group', secondary=user_group, back_populates='users'
     )
@@ -103,6 +114,15 @@ class Group(db.Model):
         'User', secondary=user_group, back_populates='groups'
     )
 
+class Role(db.Model):
+    __tablename__ = 'role'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, unique=True, nullable=False)
+
+    users = db.relationship(
+        'User', secondary=user_role, back_populates='roles'
+    )
 
 class Flowchart(db.Model):
     __tablename__ = 'flowchart'
