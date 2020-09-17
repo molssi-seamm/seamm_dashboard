8665310a2776a213090cbf94b505e87879a7cc8c
Jessica Nash <janash@vt.edu>
2020-09-17 13:32:41 -0400

    add initial protection of jobs

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,3 +1,3 @@
-from .models import Job, Flowchart, Project, User, Group
+from .models import Job, Flowchart, Project, User, Group, Role
 from .models import JobSchema, FlowchartSchema, ProjectSchema, UserSchema, GroupSchema
 from .import_jobs import import_jobs
