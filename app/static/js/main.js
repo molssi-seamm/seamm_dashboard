e2d038cd731044b440e767fc15d8b6a405718b4a
Jessica Nash <janash@vt.edu>
2020-10-01 12:12:54 -0400

    update how status is displayed on main page

--- app/static/js/main.js
+++ app/static/js/main.js
@@ -1,98 +1,31 @@
-/* global Chart, getStyle, hexToRgba */
-
 /**
  * --------------------------------------------------------------------------
- * CoreUI Free Boostrap Admin Template (v2.0.0-beta.2): main.js
- * Licensed under MIT (https://coreui.io/license)
+ * mainjs - javascript for home view
+ * 
  * --------------------------------------------------------------------------
  */
 
-/* eslint-disable no-magic-numbers */
-var MainView = function ($) {
-  
-  var mainChart = new Chart($('#main-chart'), {
-    type: 'line',
-    data: {
-      labels: ['M', 'T', 'W', 'T', 'F', 'S', 'S', 'M', 'T', 'W', 'T', 'F', 'S', 'S', 'M', 'T', 'W', 'T', 'F', 'S', 'S', 'M', 'T', 'W', 'T', 'F', 'S', 'S'],
-      datasets: [{
-        label: 'My First dataset',
-        backgroundColor: hexToRgba(getStyle('--info'), 10),
-        borderColor: getStyle('--info'),
-        pointHoverBackgroundColor: '#fff',
-        borderWidth: 2,
-        data: [165, 180, 70, 69, 77, 57, 125, 165, 172, 91, 173, 138, 155, 89, 50, 161, 65, 163, 160, 103, 114, 185, 125, 196, 183, 64, 137, 95, 112, 175]
-      }, {
-        label: 'My Second dataset',
-        backgroundColor: 'transparent',
-        borderColor: getStyle('--success'),
-        pointHoverBackgroundColor: '#fff',
-        borderWidth: 2,
-        data: [92, 97, 80, 100, 86, 97, 83, 98, 87, 98, 93, 83, 87, 98, 96, 84, 91, 97, 88, 86, 94, 86, 95, 91, 98, 91, 92, 80, 83, 82]
-      }, {
-        label: 'My Third dataset',
-        backgroundColor: 'transparent',
-        borderColor: getStyle('--danger'),
-        pointHoverBackgroundColor: '#fff',
-        borderWidth: 1,
-        borderDash: [8, 5],
-        data: [65, 65, 65, 65, 65, 65, 65, 65, 65, 65, 65, 65, 65, 65, 65, 65, 65, 65, 65, 65, 65, 65, 65, 65, 65, 65, 65, 65, 65, 65]
-      }]
-    },
-    options: {
-      maintainAspectRatio: false,
-      legend: {
-        display: false
-      },
-      scales: {
-        xAxes: [{
-          gridLines: {
-            drawOnChartArea: false
-          }
-        }],
-        yAxes: [{
-          ticks: {
-            beginAtZero: true,
-            maxTicksLimit: 5,
-            stepSize: Math.ceil(250 / 5),
-            max: 250
-          }
-        }]
-      },
-      elements: {
-        point: {
-          radius: 0,
-          hitRadius: 10,
-          hoverRadius: 4,
-          hoverBorderWidth: 3
-        }
-      }
-    }
-  });
-  var brandBoxChartLabels = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
-  var brandBoxChartOptions = {
-    responsive: true,
-    maintainAspectRatio: false,
-    legend: {
-      display: false
-    },
-    scales: {
-      xAxes: [{
-        display: false
-      }],
-      yAxes: [{
-        display: false
-      }]
-    },
-    elements: {
-      point: {
-        radius: 0,
-        hitRadius: 10,
-        hoverRadius: 4,
-        hoverBorderWidth: 3
-      }
-    } // eslint-disable-next-line no-unused-vars
+ $(document).ready( function() {
 
-  };
-  return MainView;
-}($);
-//# sourceMappingURL=main.js.map
+    let dashboardStatus = {};
+    $.ajax({
+    url: `api/status`,
+    dataType: 'json',
+    async: false,
+    success: function (data) {
+        dashboardStatus = data;
+        },
+    })
+
+    // Load info into divs
+    document.getElementById('num-jobs-running').textContent = dashboardStatus.jobs.running
+    document.getElementById('num-jobs-finished').textContent = dashboardStatus.jobs.finished
+    document.getElementById('num-jobs-queued').textContent = dashboardStatus.jobs.queued
+    document.getElementById('num-projects').textContent = dashboardStatus.projects
+    document.getElementById('num-flowcharts').textContent = dashboardStatus.flowcharts
+
+
+
+
+ })
+
