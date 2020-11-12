9ff651dc4f29e5b5b938871ba26ca4c0bdcdc0bc
Jessica Nash <janash@vt.edu>
2020-11-11 20:33:21 -0500

    add token refresh route

--- app/routes/api/auth.py
+++ app/routes/api/auth.py
@@ -4,15 +4,16 @@ Routes for REST authentication
 
 from flask import jsonify, request, Response
 from flask_jwt_extended import (create_access_token, create_refresh_token, 
-                                set_access_cookies, set_refresh_cookies)
+                                set_access_cookies, set_refresh_cookies,
+                                jwt_refresh_token_required)
 
 from app.models import User, UserSchema
 
-__all__ = ['get_auth_token']
+__all__ = ['get_auth_token', 'refresh_auth_token']
 
 def create_tokens(user):
     """
-    Create a token for a given user object.
+    Create a token for a given user object. Not an api endpoint
     """
 
     user_schema = UserSchema(many=False)
@@ -23,6 +24,9 @@ def create_tokens(user):
     return access_token, refresh_token
 
 def get_auth_token(body):
+    """
+    Write docstring TODO
+    """
 
     username = body.pop('username')
     password = body.pop('password')
@@ -35,10 +39,26 @@ def get_auth_token(body):
     if user.verify_password(password):
         access_token, refresh_token = create_tokens(user)
         
-        resp = Response()
+        resp = Response({'login': True})
         set_access_cookies(resp, access_token)
         set_refresh_cookies(resp, refresh_token)
         return resp, 200
     else:
         return jsonify({"msg": "Incorrect password."}), 400
+
+
+@jwt_refresh_token_required
+def refresh_auth_token():
+    """
+    Route for refreshing the access token.
+    """
+
+    # Create the new access token
+    current_user = get_jwt_identity()
+    access_token = create_access_token(identity=current_user)
+
+    # Set the JWT access cookie in the response
+    resp = Response({'refresh': True})
+    set_access_cookies(resp, access_token)
+    return resp, 200
 
