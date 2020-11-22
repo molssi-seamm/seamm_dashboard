ad1d748d0b1e0c1d68c1f07313d87d21f9621e75
Jessica Nash <janash@vt.edu>
2020-11-22 13:15:31 -0500

    update api tests

--- app/tests/test_api.py
+++ app/tests/test_api.py
@@ -1,66 +1,13 @@
 """
-Tests for the API
+Tests for the API (public user)
 """
-
-import pytest
 
 import os
 import json
-from datetime import date
-from dateutil import parser
 
-from flask import jsonify
+import pytest
 
-from app.routes.api.jobs import *
-from app.routes.api.flowcharts import *
 
-@pytest.mark.parametrize("createdSince, createdBefore, limit, expected_number", [
-    ("01-01-2018", None, None, 1),
-    (None, "01-01-2018", None, 2),
-    (None, "01-01-2015", None, 0),
-    (None, None, 1, 1),
-    (None, None, None, 3),
-])
-def test_get_jobs(createdSince, createdBefore, limit, expected_number, auth_client):
-    """Tests get method for api/jobs with various query strings"""
-
-    query_string = "api/jobs"
-    
-    if createdSince is not None:
-        query_string += F"?createdSince={createdSince}"
-    if createdBefore is not None:
-        query_string += F"?createdBefore={createdBefore}"
-    if limit is not None:
-        query_string += F"?limit={limit}"
-
-    response = auth_client.get(query_string)
-    jobs_received = response.json
-
-    assert len(jobs_received) == expected_number
-
-def test_get_job_by_id(client):
-    """API endpoint api/jobs/{jobID}"""
-
-    response = client.get("api/jobs/3")
-    
-    expected_response =  {
-        "flowchart_id": "ABCD",
-        "id": 3,
-        "path": "/Users/username/seamm/projects",
-        "submitted": parser.parse("2019-08-29T09:12:33.001000+00:00").replace(tzinfo=None)
-        }
-
-    received = response.data
-
-    assert False, response.data
-
-    for k in expected_response.keys():
-        if k == "submitted":
-            received[k] = parser.parse(received[k])
-          
-            assert received[k] == expected_response[k]  
-
-    assert response.status_code == 200
 
 def test_get_protected_job(client):
 
@@ -68,7 +15,7 @@ def test_get_protected_job(client):
     response = client.get("api/jobs/2")
     dbstatus = client.get("api/status")
 
-    assert response.status_code == 401, dbstatus.json['username']
+    assert response.status_code == 401, dbstatus.data
 
 def test_get_job_missing(client):
     """
@@ -89,13 +36,6 @@ def test_flowcharts_logged_out(client):
     assert len(response.json) == 0
     assert response.status_code == 200
 
-def test_flowcharts_logged_in(auth_client):
-
-    response = auth_client.get('api/flowcharts')
-
-    assert len(response[0]) == 1,'The response is'+ str(response)
-    assert response[1] == 200
-
 def test_get_flowchart_logged_out(client):
     """
     API endpoint for api/flowcharts/{flowchart_ID}
@@ -106,16 +46,6 @@ def test_get_flowchart_logged_out(client):
     response = client.get("api/flowcharts/ABCD")
     assert response.status_code == 401
 
-@pytest.mark.usefixtures("authenticated_request")
-def test_get_flowchart_logged_in():
-    """
-    Test for api/flowcharts/{flowchart_ID} when logged in and owner of flowchart
-    """
-
-    response = get_flowchart('ABCD')
-
-    assert response[1] == 200
-
 def test_get_cytoscape(client):
     """
     API endpoint for api/flowcharts/{flowchart_ID}/cytoscape when not logged in.
@@ -125,29 +55,6 @@ def test_get_cytoscape(client):
 
     response = client.get("api/flowcharts/ABCD/cytoscape")
     assert response.status_code == 401
-
-@pytest.mark.usefixtures("authenticated_request")
-def test_get_cytoscape_logged_in():
-    """
-    Test for api/flowcharts/{flowchart_ID}/cytoscape
-    """
-
-    response = get_cytoscape("ABCD")
-    assert response[1] == 201
-
-@pytest.mark.usefixtures("authenticated_request")
-def test_update_job():
-    """Check put method of api/jobs/{job_ID}"""
-
-    original_info = get_job(1)[0]
-    assert original_info["status"].lower() == "finished"
-    
-    response = update_job(1, {"status": "submitted"})
-    
-    assert response.status_code == 201
-
-    new_info = get_job(1)[0]
-    assert new_info["status"]  == "submitted"
 
 def test_update_job_not_authenticated(client):
     """Check put method of api/jobs/{job_ID}"""
@@ -162,19 +69,5 @@ def test_add_job(client):
     """Check post method of api/jobs/"""
     # Ask Paul
     assert False
-
-@pytest.mark.usefixtures("admin_request")
-def test_delete_job(project_directory):
-    """Check delete method of api/jobs/{jobID}"""
-
-    expected_path = os.path.join(project_directory, "Job_000002")
-
-    assert os.path.exists(expected_path)
-
-    response = delete_job(2)
-    
-    assert response.status_code == 200
-
-    assert not os.path.exists(expected_path)
 
 
---
+++ app/tests/test_api_admin.py
@@ -0,0 +1,19 @@
+"""
+Tests for the API (admin user)
+"""
+
+
+import os
+
+def test_delete_job(admin_client, project_directory):
+    """Check delete method of api/jobs/{jobID}"""
+
+    expected_path = os.path.join(project_directory, "Job_000002")
+
+    assert os.path.exists(expected_path)
+
+    response = admin_client.delete("api/jobs/2")
+
+    assert response.status_code == 200
+
+    assert not os.path.exists(expected_path)
