da094850bced33e9ce7784249e5e0e9db8bd8c72
Jessica Nash <janash@vt.edu>
2021-01-20 23:55:05 -0500

    groups api endpoiont returns usernames

--- app/routes/api/groups.py
+++ app/routes/api/groups.py
@@ -8,13 +8,18 @@ from app import authorize
 from app.models import Group, GroupSchema
 
 @jwt_optional
-@authorize.has_role("admin", "manager")
+@authorize.has_role("admin", "group manager")
 def get_groups():
 
     groups = Group.query.all()
 
+    # usernames = [ [ user.username for user in group.users ] for group in groups ]
+
     group_schema = GroupSchema(many=True)
 
     groups = group_schema.dump(groups)
+
+    # for i, group in enumerate(groups):
+    #    group["usernames"] = usernames[i]
 
     return groups, 200
