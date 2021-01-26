c0556a2377512e22ef1845a6d4d57c1adb832844
Jessica Nash <janash@vt.edu>
2021-01-26 17:03:28 -0500

    add factory for generating association mixin

--- app/models/acl_models.py
+++ app/models/acl_models.py
@@ -5,7 +5,7 @@ from sqlalchemy.ext.declarative import declared_attr
 
 from flask_authorize.mixins import PipedList
 
-def generate_association_table(table_name, entity, resource):
+def generate_association_table(entity, resource):
     """
 
     """
@@ -13,23 +13,38 @@ def generate_association_table(table_name, entity, resource):
     entity_tablename = entity.__tablename__
     resource_tablename = resource.__tablename__
 
-    class PermissionsAssociation(db.Model):
-        __tablename__ = table_name
+    class PermissionsAssociationMixin:
+        __tablename__ = f"{entity_tablename}_{resource_tablename}_association"
 
-        locals()[f"{entity_tablename[:-1]}_id"] = db.Column(db.Integer, db.ForeignKey(f"{entity_tablename}.id"), primary_key=True)
-        locals()[f"{resource_tablename[:-1]}_id"] = db.Column(db.Integer, db.ForeignKey(f"{resource_tablename}.id"), primary_key=True)
+        @declared_attr
+        def entity_id(cls):
+            return db.Column(db.Integer, db.ForeignKey(f"{entity_tablename}.id"), primary_key=True)
+        
+        @declared_attr
+        def resource_id(cls):
+            return db.Column(db.Integer, db.ForeignKey(f"{resource_tablename}.id"), primary_key=True)
+        
+        @declared_attr
+        def entity_relationship(cls):
+            return db.relationship(f"{entity.__name__}", backref=f"{resource_tablename}")
+        
+        @declared_attr
+        def resource_relationship(cls):
+            db.relationship(f"{resource.__name__}", backref=f"{entity_tablename}")
+
+        locals()[f"{entity_tablename[:-1]}_id"] = entity_id
+        locals()[f"{resource_tablename[:-1]}_id"] = resource_id
         permissions = db.Column(PipedList)
 
-        
-        locals()[f"{entity_tablename[:-1]}"] = db.relationship(f"{entity.__name__}", back_populates=f"{resource_tablename}")
-        locals()[f"{resource_tablename[:-1]}"] = db.relationship(f"{resource.__name__}", back_populates=f"{entity_tablename}")
+        locals()[f"{entity_tablename[:-1]}"] = entity_relationship
+        locals()[f"{resource_tablename[:-1]}"] = resource_relationship
     
-    _permissions_association = PermissionsAssociation
+    _permissions_mixin = PermissionsAssociationMixin
 
     # Set-up relationshps on associated tables.
-    setattr(entity, f"{resource_tablename}", db.relationship("PermissionsAssociation", lazy="dynamic", back_populates=f"{entity_tablename[:-1]}"))
-    setattr(resource, f"{entity_tablename}", db.relationship("PermissionsAssociation", lazy="dynamic", back_populates=f"{resource_tablename[:-1]}"))
+    #
+    #setattr(resource, f"{entity_tablename}", db.relationship("PermissionsAssociation", lazy="dynamic", back_populates=f"{resource_tablename[:-1]}"))
 
-    return _permissions_association
+    return _permissions_mixin
         
     
