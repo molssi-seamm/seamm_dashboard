43a30fd472ba124f4f8663595109180fb33fb905
Jessica Nash <janash@vt.edu>
2021-02-08 14:55:00 -0500

    fix imports for patch

--- app/flask_authorize_patch.py
+++ app/flask_authorize_patch.py
@@ -1,3 +1,5 @@
+import six
+
 from sqlalchemy import Column, ForeignKey, Integer, or_
 from sqlalchemy.orm import relationship, backref
 
@@ -5,7 +7,15 @@ from sqlalchemy.ext.declarative import declared_attr
 
 from flask_authorize.mixins import PipedList, PermissionsMixin
 
-from flask_authorize.plugin import Authorize
+from flask_authorize.plugin import (
+    Authorizer,
+    user_is_restricted,
+    user_is_allowed,
+    has_permission,
+    user_has_role
+)
+
+from flask import current_app
 
 __all__ = ["generate_association_table", "AccessControlPermissionsMixin", "Authorize"]
 
@@ -101,104 +111,108 @@ class AccessControlPermissionsMixin(PermissionsMixin):
 
         return or_(ret, or_(*clauses))
 
+
 def allowed(self, *args, **kwargs):
+    from flask_authorize.plugin import CURRENT_USER
 
-        # look to flask-login for current user
-        user = kwargs.get('user')
-        if user is None:
-            user = CURRENT_USER()
+    # look to flask-login for current user
+    user = kwargs.get("user")
+    if user is None:
+        user = CURRENT_USER()
 
-        # otherwise, use current user method
-        elif isinstance(user, types.FunctionType):
-            user = user()
+    # otherwise, use current user method
+    elif isinstance(user, types.FunctionType):
+        user = user()
 
-        # don't allow anything for anonymous users
-        if user is None:
-            if not current_app.config["AUTHORIZE_ALLOW_ANONYMOUS_ACTIONS"]:
-                return False
+    # don't allow anything for anonymous users
+    if user is None:
+        if not current_app.config["AUTHORIZE_ALLOW_ANONYMOUS_ACTIONS"]:
+            return False
 
-        # authorize if user has relevant role
-        if len(self.has_role):
-            if user_has_role(user, self.has_role):
-                return True
-            elif not len(self.permission) and not len(self.create):
-                return False
-
-        # authorize if user has relevant group
-        if len(self.in_group):
-            if user_in_group(user, self.in_group):
-                return True
-            elif not len(self.permission) and not len(self.create):
-                return False
-
-        # authorize create privileges based on access
-        if len(self.create):
-            for model in self.create:
-                if user_is_restricted(user, ['create'], model) or \
-                   not user_is_allowed(user, ['create'], model):
-                    return False
-
-        # return if no additional permission check needed
-        if len(self.permission) == 0:
+    # authorize if user has relevant role
+    if len(self.has_role):
+        if user_has_role(user, self.has_role):
             return True
+        elif not len(self.permission) and not len(self.create):
+            return False
 
-        # check permissions on individual instances - all objects
-        # must have authorization to proceed.
-        operation = set(self.permission)
-        for arg in args:
+    # authorize if user has relevant group
+    if len(self.in_group):
+        if user_in_group(user, self.in_group):
+            return True
+        elif not len(self.permission) and not len(self.create):
+            return False
 
-            if not isinstance(arg.__class__, six.class_types):
-                continue
-
-            # check role restrictions/allowances
-            if user_is_restricted(user, operation, arg):
+    # authorize create privileges based on access
+    if len(self.create):
+        for model in self.create:
+            if user_is_restricted(user, ["create"], model) or not user_is_allowed(
+                user, ["create"], model
+            ):
                 return False
 
-            if not user_is_allowed(user, operation, arg):
-                return False
-
-            # only check permissions for items that have set permissions
-            check = current_app.config['AUTHORIZE_IGNORE_PROPERTY']
-            if hasattr(arg, check) and not getattr(arg, check):
-                continue
-            if not hasattr(arg, 'permissions'):
-                continue
-
-            # check other permissions
-            check = arg.permissions.get('other', [])
-            permitted = has_permission(operation, check)
-
-            # check user permissions
-            if hasattr(arg, 'owner'):
-                if arg.owner == user:
-                    check = arg.permissions.get('owner', [])
-                    permitted |= has_permission(operation, check)
-
-            # check group permissions
-            if hasattr(arg, 'group'):
-                if hasattr(user, 'groups'):
-                    if arg.group in user.groups:
-                        check = arg.permissions.get('group', [])
-                        permitted |= has_permission(operation, check)
-
-            # check special permissions
-            # should probably figure this out instead of cheating.
-            operation = list(operation)[0]
-            
-            if hasattr(arg, "special_users") and CURRENT_USER():
-                for assoc in arg.special_users.all():
-                    if CURRENT_USER().id == assoc.entity_id:
-                        permitted |= operation in assoc.permissions
-
-            if hasattr(arg, "special_groups") and CURRENT_USER():
-                for group in arg.special_groups.all():
-                    if group.entity_id in [x.id for x in CURRENT_USER().groups ]:
-                        
-                        permitted |= operation in group.permissions
-
-            if not permitted:
-                return False
-
+    # return if no additional permission check needed
+    if len(self.permission) == 0:
         return True
 
-Authorize.allowed = allowed
+    # check permissions on individual instances - all objects
+    # must have authorization to proceed.
+    operation = set(self.permission)
+    for arg in args:
+
+        if not isinstance(arg.__class__, six.class_types):
+            continue
+
+        # check role restrictions/allowances
+        if user_is_restricted(user, operation, arg):
+            return False
+
+        if not user_is_allowed(user, operation, arg):
+            return False
+
+        # only check permissions for items that have set permissions
+        check = current_app.config["AUTHORIZE_IGNORE_PROPERTY"]
+        if hasattr(arg, check) and not getattr(arg, check):
+            continue
+        if not hasattr(arg, "permissions"):
+            continue
+
+        # check other permissions
+        check = arg.permissions.get("other", [])
+        permitted = has_permission(operation, check)
+
+        # check user permissions
+        if hasattr(arg, "owner"):
+            if arg.owner == user:
+                check = arg.permissions.get("owner", [])
+                permitted |= has_permission(operation, check)
+
+        # check group permissions
+        if hasattr(arg, "group"):
+            if hasattr(user, "groups"):
+                if arg.group in user.groups:
+                    check = arg.permissions.get("group", [])
+                    permitted |= has_permission(operation, check)
+
+        # check special permissions
+        # should probably figure this out instead of cheating.
+        operation = list(operation)[0]
+
+        if hasattr(arg, "special_users") and CURRENT_USER():
+            for assoc in arg.special_users.all():
+                if CURRENT_USER().id == assoc.entity_id:
+                    permitted |= operation in assoc.permissions
+
+        if hasattr(arg, "special_groups") and CURRENT_USER():
+            for group in arg.special_groups.all():
+                if group.entity_id in [x.id for x in CURRENT_USER().groups]:
+
+                    permitted |= operation in group.permissions
+
+        if not permitted:
+            return False
+
+    return True
+
+
+Authorizer.allowed = allowed
