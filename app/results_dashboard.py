ee3f1ba7c0bd678399763612fb80c77f72086054
Paul Saxe <paul@thesaxes.net>
2021-03-26 17:58:16 -0400

    Running as a package. Unfortunately the import name is 'app', so more changes needed.

--- app/results_dashboard.py
+++ app/results_dashboard.py
@@ -4,7 +4,8 @@ from app import create_app
 
 def run():
     app = create_app()
-    app.run(debug=True, use_reloader=True)
+    # app.run(debug=True, use_reloader=True)
+    app.run(debug=False, use_reloader=False)
 
 
 if __name__ == "__main__":
