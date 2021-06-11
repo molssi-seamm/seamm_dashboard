62c29a6d0f30f0a74ff4a299e1659e8cbfd2dc2f
Jessica Nash <janash@vt.edu>
2021-06-11 15:51:15 -0400

    add error check to job upload form

--- seamm_dashboard/routes/jobs/forms.py
+++ seamm_dashboard/routes/jobs/forms.py
@@ -2,6 +2,8 @@ from flask_wtf import FlaskForm
 from flask_wtf.file import FileField
 from wtforms import StringField, TextAreaField, SubmitField
 
+from wtforms.validators import DataRequired
+
 
 class EditJob(FlaskForm):
     name = StringField("Job Title")
@@ -10,6 +12,6 @@ class EditJob(FlaskForm):
 
 
 class ImportJob(FlaskForm):
-    outfile = FileField("job_data.json File")
+    outfile = FileField("job_data.json File", validators=[DataRequired()])
     title = StringField("Job Title")
     submit = SubmitField("Import Job")
