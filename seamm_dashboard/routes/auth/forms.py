7a07e4a9c4784d79c51c45749bf10a1cb8e9d03b
Paul Saxe <paul@thesaxes.net>
2021-11-20 16:36:36 -0500

    EmailField may have moved?

--- seamm_dashboard/routes/auth/forms.py
+++ seamm_dashboard/routes/auth/forms.py
@@ -1,6 +1,7 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
-from wtforms.fields.html5 import EmailField
+# from wtforms.fields.html5 import EmailField
+from wtforms.fields import EmailField
 
 from wtforms.validators import DataRequired, Length, EqualTo, Email
 
