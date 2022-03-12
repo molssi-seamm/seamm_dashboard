e3fb7ac012067686bf74e3c7af0ff2cbf5325796
Jessica Nash <janash@vt.edu>
2022-03-12 03:36:21 +0000

    add validators to project form

--- seamm_dashboard/routes/jobs/forms.py
+++ seamm_dashboard/routes/jobs/forms.py
@@ -2,11 +2,22 @@ from flask_wtf import FlaskForm
 from flask_wtf.file import FileField
 from wtforms import StringField, TextAreaField, SubmitField
 
-from wtforms.validators import DataRequired
+from wtforms.validators import DataRequired, Length, Regexp
 
 
 class EditJob(FlaskForm):
-    name = StringField("Job Title")
+    name = StringField(
+        "Job Title",
+        validators=[
+            DataRequired(),
+            Length(1, 64),
+            Regexp(
+                "^[A-Za-z][A-Za-z0-9_. ]*$",
+                0,
+                "Job titles must have only letters, numbers, dots, spaces, or underscores",
+            ),
+        ],
+    )
     notes = TextAreaField("Description")
     submit = SubmitField("Update Job")
 
--- seamm_dashboard/routes/projects/forms.py
+++ seamm_dashboard/routes/projects/forms.py
@@ -1,15 +1,39 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, TextAreaField, SubmitField
 
+from wtforms.validators import DataRequired, Length, Regexp
+
 
 class EditProject(FlaskForm):
-    name = StringField("Project Title")
+    name = StringField(
+        "Project Title",
+        validators=[
+            DataRequired(),
+            Length(1, 64),
+            Regexp(
+                "^[A-Za-z][A-Za-z0-9_. ]*$",
+                0,
+                "Projects must have only letters, numbers, dots, spaces, or underscores",
+            ),
+        ],
+    )
     notes = TextAreaField("Description")
     submit = SubmitField("Update Project")
 
 
 class AddProject(FlaskForm):
-    name = StringField("Project Title")
+    name = StringField(
+        "Project Title",
+        validators=[
+            DataRequired(),
+            Length(1, 64),
+            Regexp(
+                "^[A-Za-z][A-Za-z0-9_. ]*$",
+                0,
+                "Projects must have only letters, numbers, dots, spaces, or underscores",
+            ),
+        ],
+    )
     notes = TextAreaField("Description")
     submit = SubmitField("Create Project")
 
