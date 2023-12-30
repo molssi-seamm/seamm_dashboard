8c78a8539d534f4f298eb84c5c0cc3ccd30b68ca
Paul Saxe <paul@thesaxes.net>
2023-12-30 15:57:43 -0500

    Checking if localhost is causing problems in CI.

--- seamm_dashboard/results_dashboard.py
+++ seamm_dashboard/results_dashboard.py
@@ -2,8 +2,6 @@
 from seamm_dashboard import create_app, options
 from waitress import serve
 
-production = False
-
 
 def run():
     app = create_app()
@@ -13,7 +11,7 @@ def run():
         app.run(debug=True, use_reloader=True)  
     else:   
         # serve using waitress
-        if options["localhost"]:
+        if False or options["localhost"]:
             serve(app, listen=f"localhost:{options['port']}", threads=12)
         else:
             serve(app, port=options["port"], threads=12)
